66423f730e6d77920832879b2ff1be05432d08c7
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-05-01 01:14:03 +0100

    feat: criando cliente udp

---
+++ cliente-server-udp/cliente-udp.py
@@ -0,0 +1,25 @@
+import socket
+
+
+# AF_INET ipv4
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("client Socket criado com sucesso")
+
+
+host = "localhost"
+port = 5433
+message = "Hello Server!!!"
+
+
+try:
+    print('Cliente: ' + message)
+    s.sendto(message.encode(), (host, port))
+
+    data, server = s.recvfrom(4096)
+    data = data.decode()
+    print("clinte: " + data)
+finally:
+    print("Cliente: Fechando a conexao")
+    s.close()
